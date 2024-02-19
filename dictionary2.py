442941deefb3dc58d9f8a55c630ff1d337408e26
swatijamgade <155985641+swatijamgade@users.noreply.github.com>
2024-02-19 16:45:57 +0000

    Add files via upload

--- dictionary2.py
+++ dictionary2.py
@@ -1,27 +1,16 @@
 # Write a program to sum all the values of a dictionary
-# marks={"m1":92 , "m2":56 , "m3":88 , "m4":97 , "m5":89}
-# values = marks.values()
-# total = sum(values)
-# print(total)
+marks={"m1":92 , "m2":56 , "m3":88 , "m4":97 , "m5":89}
+values = marks.values()
+total = sum(values)
+print(total)
 
 # Write a program to get the maximum and minimum value of dictionary.
 # marks={"m1":78 , "m2":89 , "m3":64 , "m4":35 , "m5":71}
-
-# v = marks.values()
-# maxi = max(v)
-# mini = min(v)
-# print(maxi)
-# print(mini)
-
-# # Write a program to sort dictionary by values (Ascending/ Descending)
-# d={"m1":78 , "m2":89 , "m3":64 , "m4":35 , "m5":71}
-# values = sorted(x for x in d.values())
-# sorted_keys = []
-# for item in values:
-#     for key in d:
-#         if d[keys] == item:
-#             sorted_keys.append(ke  ny) 
-exit
+v = marks.values()
+maxi = max(v)
+mini = min(v)
+print(maxi)
+print(mini)
 
 # Write a program to print keys and values of dictionary separatly
 student = {
@@ -36,10 +25,10 @@ keylist =[key for key in student]
 print(keylist)
 
 # Write a Python program to Rename keys of a dictionary
-# student = {
-# 	"Name": "Tara",
-# 	"RollNo":130046, 
-# }
+student = {
+	"Name": "Tara",
+	"RollNo":130046, 
+}
 student["name1"] = student.pop("Name")
 student["rOLL"] = student.pop("RollNo")
 print(student)
@@ -47,7 +36,6 @@ print(student)
 # Write a Python program to Delete a list of keys from a dictionary
 # keys_to_remove = ["City","Gender"]
 
-# Given - 
 student = {
 	"Name": "Tara",
 	"RollNo":130046, 
